27ef8dc955358333d61fbb57e50abd3ea7238d6d
CaioQC <129068301+CaioQC@users.noreply.github.com>
2023-09-14 09:47:50 -0300

    Update Atividade_1.c

--- Atividade_1.c
+++ Atividade_1.c
@@ -3,14 +3,13 @@
 int main() {
     int ano;
 
-    printf("Digite um ano entre 1896 e 2022: ");
     scanf("%d", &ano);
 
-    if ((ano >= 1896 && ano <= 2022) && (ano % 4 == 0)) {
+    if ((ano >= 1896 && ano <= 2022) && (ano % 4 == 0) && (ano != 1916) && (ano != 1940) && (ano != 1944)) {
         printf("Os Jogos Olímpicos de Verão ocorreram no ano de %d.\n", ano);
     } 
     
-    else if (ano >= 1930 && (ano - 1930) % 4 == 0){
+    else if (ano >= 1930 && (ano - 1930) % 4 == 0 && (ano != 1942) && (ano != 1946)) {
         printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
     } 
 
