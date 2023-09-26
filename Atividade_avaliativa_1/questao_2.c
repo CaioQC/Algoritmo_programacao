f56f50786d3721c01bfa0e1506a23cb70f1fc94b
CaioQC <129068301+CaioQC@users.noreply.github.com>
2023-09-26 16:16:03 -0300

    Update questao_2.c

--- Atividade_avaliativa_1/questao_2.c
+++ Atividade_avaliativa_1/questao_2.c
@@ -19,7 +19,7 @@ int main(){
         dividendo = dividendo / 10; 
     }
 
-    printf("A soma dos algarismos de %d eh igual a %d\n", numero, soma);
+    printf("A soma dos algarismos de %d eh %d\n", numero, soma);
 
     return 0;
 }
