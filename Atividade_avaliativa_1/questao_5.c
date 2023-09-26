78f97894a5de2901aa9b90120e8a4d19d8c26670
CaioQC <129068301+CaioQC@users.noreply.github.com>
2023-09-26 19:36:58 -0300

    Update questao_5.c

--- Atividade_avaliativa_1/questao_5.c
+++ Atividade_avaliativa_1/questao_5.c
@@ -14,23 +14,23 @@ int main(){
 
     else if (numero1 > numero2){
         printf("%d eh maior que %d\n", numero1, numero2);
-        if((numero1 % numero2) == 0){
+        if((numero2 != 0) && (numero1 % numero2) == 0){
             printf("%d eh multiplo de %d\n", numero1, numero2);
         }
 
         else{
-            printf("%d não eh multiplo de %d\n", numero1, numero2);
+            printf("%d nao eh multiplo de %d\n", numero1, numero2);
         }
     }
 
     else if(numero1 < numero2){
         printf("%d eh maior que %d\n", numero2, numero1);
-        if((numero2 % numero1) == 0){
+        if((numero1 != 0) && (numero2 % numero1) == 0){
             printf("%d eh multiplo de %d\n", numero2, numero1);
         }
 
         else{
-            printf("%d não eh multiplo de %d\n", numero2, numero1);
+            printf("%d nao eh multiplo de %d\n", numero2, numero1);
         }
     }
     return 0;
